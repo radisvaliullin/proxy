fed61a1d767a454c3d87d00e3b4044173380e4df
radis <radisvaliullin@gmail.com>
2023-11-29 23:58:05 -0800

    proxy-base: add config package

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"log"
 
+	"github.com/radisvaliullin/proxy/pkg/config"
 	"github.com/radisvaliullin/proxy/pkg/proxy"
 )
 
 func main() {
 
-	p := proxy.New(proxy.Config{})
+	// get config
+	config, err := config.New()
+	if err != nil {
+		log.Fatalf("main: get config: %v", err)
+	}
+	log.Printf("main: config: %+v", config)
+
+	// init proxy and start
+	p := proxy.New(config.Proxy)
 	if err := p.Start(); err != nil {
 		log.Fatalf("main: proxy start: %v", err)
 	}
